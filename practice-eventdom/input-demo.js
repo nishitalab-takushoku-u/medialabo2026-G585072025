9b2717a66375f5ce77ba57f01914d86ae0ea75ed
G585072025 <258507@st.takushoku-u.ac.jp>
2026-05-14 15:50:28 +0900

    5/14

--- practice-eventdom/input-demo.js
+++ practice-eventdom/input-demo.js
@@ -1,7 +1,8 @@
 function greeting(){
     let i = document.querySelector('input[name="shimei"]');
+    let shimei = i.value;
     let p = document.querySelector('p#message');
-    p.textContent = 'こんにちは, '+i.value+'さん';
+    p.textContent = 'こんにちは, '+shimei+'さん';
 }
 
 let b = document.querySelector('button#print');
