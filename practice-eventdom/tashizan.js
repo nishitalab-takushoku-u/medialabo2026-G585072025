d37f5c3d7c151a20c7d846ac6649f3960e70ac7b
G585072025 <258507@st.takushoku-u.ac.jp>
2026-05-14 16:27:17 +0900

    5/14

--- practice-eventdom/tashizan.js
+++ practice-eventdom/tashizan.js
@@ -1,14 +1,13 @@
-function calculate(){
- let a = document.querySelector('input[name="right"]');
- let num1 = Number(a);
- let b = ducument.querySelector('input[name="left"]');
- let num2 = Number(b);
-
- let result = num1+num2;
- let p = ducument.querySelector('p#answer');
- p.textContent = result;
+function calculate() {
+    let a = document.querySelector('input[name="right"]');
+    let num1 = Number(a.value);
+    let b = document.querySelector('input[name="left"]');
+    let num2 = Number(b.value);
+    let result = num1 + num2;
+    let p = document.querySelector('p#answer');
+    p.textContent = result;
 }
 
-let i = document.querySelector('button#Calc');
+let i = document.querySelector('button#calc');
 
-i.addEventListener('click',Calculate);
+i.addEventListener('click', calculate);
